ca926f9b173779dca3dfc09f0fb2c571fe019f72
Jack <jkswel@gmail.com>
2025-04-14 13:17:20 -0600

    Comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const client = generateClient<Schema>();
 
 function Admin() {
   const { signOut } = useAuthenticator();
+
+  //State to hold the list of all tutors retrieved from the database
   const [tutors, setTutors] = useState<Array<Schema["Tutor"]["type"]>>([]);
 
   useEffect(() => {
@@ -28,8 +30,8 @@ function Admin() {
   };
 
   function deleteTutor(id: string) {
-      client.models.Tutor.delete({ id })
-    }
+    client.models.Tutor.delete({ id }, { authMode: "userPool" });
+  }
 
     return (
       <main>
@@ -100,7 +102,6 @@ function GetUser() {
     async function fetchAndSetUserGroup() { // Use an async function to fetch the session so we can wait for it to resolve before fetching
       try {
         const session = await fetchAuthSession(); // Fetch session when it is available
-        // Ensure we safely extract the token payload.
         const payload = session.tokens?.accessToken?.payload;
         console.log("Token payload:", payload);
         const groups = (payload?.["cognito:groups"] as string[]) || [];
@@ -118,7 +119,6 @@ function GetUser() {
         setUserGroup("Unknown");
       }
     }
-
     fetchAndSetUserGroup();
   }, []);
 
@@ -138,63 +138,15 @@ function GetUser() {
 function App() {
   return (
     <Authenticator>
-      {({ signOut}) => (
+      
         <main style={{ padding: "2rem" }}>
           {/* Render the dashboard component based on the user's group */}
           <GetUser />
           {/* Sign out button */}
-          <button onClick={signOut}>Sign out</button>
         </main>
-      )}
+      
     </Authenticator>
   );
 }
-
-
-// function App() {
-//   const { signOut } = useAuthenticator();
-//   // Since user is not being used, the build will fail even though it is just a warning.
-//   // const { user, signOut } = useAuthenticator();
-
-//   const [tutors, setTutors] = useState<Array<Schema["Tutor"]["type"]>>([]);
-
-//   useEffect(() => {
-//     client.models.Tutor.observeQuery().subscribe({
-//       next: (data) => setTutors([...data.items]),
-//     });
-//   }, []);
-
-//   function createTutor() {
-//     client.models.Tutor.create({ firstName: window.prompt("First Name"), lastName: window.prompt("Last Name") });
-//   }
-
-    
-  // function deleteTutor(id: string) {
-  //   client.models.Tutor.delete({ id })
-  // }
-
-  // return (
-  //   <main>
-  //     {/* <h1>{user?.signInDetails?.loginId}'s Todos </h1> */}
-  //     <center><h1>Tutors</h1></center>
-  //     <button onClick={createTutor}>+ new Tutor</button>
-  //     <ul>
-  //       {tutors.map((Tutor) => (
-  //         <li onClick={() => deleteTutor(Tutor.id)} key={Tutor.id}>{Tutor.firstName}, {Tutor.lastName}</li>
-  //       ))}
-  //     </ul>
-  //     <div>
-  //       🥳 App successfully hosted. Try creating a new tutor.
-  //       <br />
-  //       <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-  //         Review next step of this tutorial.
-  //       </a>
-  //     </div>
-  //     <button onClick={signOut}>Sign out</button>
-  //   </main>
-  // );
-// }
-
-
 
 export default App;
